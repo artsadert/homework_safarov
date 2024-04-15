5ed5d69239af4ead752e058f264941136224f067
artsadert <artsadert@gmail.com>
2024-04-15 09:26:06 +0300

    fix sadertdinov.h

--- src/sadertdinov.h
+++ src/sadertdinov.h
@@ -3,8 +3,6 @@ void insertInArray(int data[], int& size, int position, int num);
 void insertInArray(double data[], int& size, int position, double num);
 void eraseFromArray(int data[], int& size, int position);
 void eraseFromArray(double data[], int& size, int position);
-void insertInArray(int data[], int& size, int position, int num);
-void insertInArray(double data[], int& size, int position, double num);
 void sortFromArray(int data[], int size, int position);
 void matrixMultiplier(int a[][100], int n1, int m1, int b[][100], int n2, int m2, int c[][100] );
 int minSumArray(int data[][100], int n, int m, bool row=true);
